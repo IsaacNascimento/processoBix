27dff131ba8e1c6fadec55f6670de11e0a240fdc
isaac.nascimento <isaacs.nascimento17@gmail.com>
2023-06-27 00:57:57 -0300

    [BUG FIX]: Private router controller

--- front/src/shared/hooks/useAuth.js
+++ front/src/shared/hooks/useAuth.js
@@ -2,8 +2,8 @@ import TokenService from "../../utils/api/base/tokenServicer";
 
 const useAuth = () => {
   const user = TokenService.getSession();
-  // const isLoggedIn = user ? true : false;
-  return true;
+  const isLoggedIn = user ? true : false;
+  return isLoggedIn;
 };
 
 export { useAuth };
